63f60d2b82f3cd506a40baf5009b6a0a0aa2022c
PawelHeleniak <pawel.heleniak01@gmail.com>
2023-03-06 23:15:13 +0100

    Revert "separation scss to components"
    This reverts commit 51751476df1e678e4bbdba47f1bd70ee0d9aa9fa.

--- src/global-scripts/main.js
+++ src/global-scripts/main.js
@@ -2,3 +2,25 @@
 /// Here we import all the global JavaScript files we need for our project.
 
 import "../global-styles/style.scss";
+
+//Marks active music bar
+let boxes = document.querySelectorAll(".music-list-box");
+const handleActive = (e) => {
+  boxes.forEach((box) => {
+    box.classList.remove("active");
+  });
+  e.currentTarget.classList.add("active");
+};
+boxes.forEach((box) => {
+  box.addEventListener("click", handleActive);
+});
+
+//Expands the list
+let items = document.querySelectorAll(".music-list-box-menu .menu-button");
+let dropDown = document.querySelector(".drop-down-menu");
+const handleOpenMenu = (e) => {
+  dropDown.classList.add("active");
+};
+items.forEach((item) => {
+  item.addEventListener("click", handleOpenMenu);
+});
--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -3,24 +3,6 @@
 
 import "../styles/home-page.scss";
 
-//Marks active music bar
-let boxes = document.querySelectorAll(".music-list-box");
-const handleActive = (e) => {
-  boxes.forEach((box) => {
-    box.classList.remove("active");
-  });
-  e.currentTarget.classList.add("active");
-};
-boxes.forEach((box) => {
-  box.addEventListener("click", handleActive);
-});
+import { setupCounter } from "../../../global-scripts/scripts/counter/counter.js";
 
-//Expands the list
-let items = document.querySelectorAll(".music-list-box-menu .menu-button");
-let dropDown = document.querySelector(".drop-down-menu");
-const handleOpenMenu = (e) => {
-  dropDown.classList.add("active");
-};
-items.forEach((item) => {
-  item.addEventListener("click", handleOpenMenu);
-});
+setupCounter(document.querySelector("#counter"));
